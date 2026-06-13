ef7b71ffc56a0dbf6ea6685c6b9b005bae3daf8a
Swetony <swetonyancelmo@gmail.com>
2026-06-13 09:49:29 -0300

    feat: add task route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { TaskListComponent } from './features/tasks/task-list/task-list.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'tasks', component: TaskListComponent, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
